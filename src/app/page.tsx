4faa036acbb1960221166a76d901741e1541cbc3
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-02-29 01:44:22 +0530

    about us section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import { HeroParallax } from '@/components/ui/hero-parallax';
 import Navbar from '../components/Navbar';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import Image from 'next/image';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination, Autoplay } from 'swiper/modules';
+import Heading from '@/components/ui/heading';
+import Link from 'next/link';
 
 
 
@@ -44,7 +46,7 @@ export default function Home() {
 
       {/* <HeroParallax products = {products}/> */}
 
-      <section className='w-full h-screen'>
+      <section className='w-full '>
         <Swiper pagination={{ clickable: true }} modules={[Pagination, Autoplay]}
           autoplay={{
             delay: 1500,
@@ -65,6 +67,37 @@ export default function Home() {
           ))}
         </Swiper>
       </section>
+
+
+     
+
+      <section >
+        <Heading heading='About Us' text='What is Mod Equipment all about?'/>
+        <div className='flex flex-col md:flex-row px-5 md:px-10 gap-8 '>
+        <Image src='/5.jpg' width={600} height={1000} alt="about us image"/>
+
+<div className='flex flex-col justify-center'>
+          <p className='text-lg'>MOD Kitchen Equipment Pvt Ltd is a top player in the category Kitchen Equipment Dealers in the Delhi. This well-known establishment acts as a one-stop destination servicing customers both local and from other parts of Delhi. Over the course of its journey, this business has established a firm foothold in its industry. <br/> <br/>The belief that customer satisfaction is as important as their products and services, have helped this establishment garner a vast base of customers, which continues to grow by the day. This business employs individuals that are dedicated towards their respective roles and put in a lot of effort to achieve the common vision and larger goals of the company.</p>
+            <div className='flex md:gap-7 gap-3 my-5 text-center flex-col md:flex-row text-secondary text-lg '>
+              <Link href={'/e-catalog'} className='bg-foreground px-5 py-2 rounded-md md:w-2/5'>Read More</Link>
+              <Link href={'/contact'} className='bg-primary px-5 py-2 rounded-md md:w-2/5'>Contact Us</Link>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section className='my-10 md:my-24'>
+        <div className='mx-auto max-w-screen-xl px-10 bg-primary h-60 md:h-52 text-secondary md:text-4xl text-2xl text-center flex justify-center items-center'>
+          <h3>We produce solutions with our products for the commercial kitchens of more than <b>130</b> countries in <b>5</b> continents.</h3>
+        </div>
+      </section>
+
+
+
+      <section className='h-screen w-full'></section>
+    
+
       
 
     </main>
--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -1,12 +1,12 @@
 export default function Heading({heading, text}: {heading: string, text: string}) {
     return (
-        <div className="text-primary font-poppins my-10 flex flex-col items-center">
-            <div className="flex gap-3 items-center justify-center w-[45%]">
+        <div className="text-primary font-mont my-10 flex flex-col items-center text-center px-5">
+            <div className="flex gap-3 items-center justify-center md:w-[45%] w-full">
                 <div className="w-full h-[3px] bg-primary rounded-full" />
-                <h1 className="text-2xl uppercase text-nowrap text-foreground">{heading}</h1>
+                <h2 className="md:text-2xl text-lg uppercase text-nowrap text-foreground">{heading}</h2>
                 <div className="w-full h-[3px] bg-primary rounded-full" />
             </div>
-            <p className="text-5xl font-semibold">{text}</p>
+            <h3 className="md:text-5xl text-3xl font-semibold">{text}</h3>
         </div>
     )
 }
