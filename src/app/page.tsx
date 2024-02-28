c5162cfc57c711603469049f869d797ac66fab77
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-02-28 17:23:33 +0530

    hero section made. animation migrated to /test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,38 @@
+'use client'
 import { HeroParallax } from '@/components/ui/hero-parallax';
 import Navbar from '../components/Navbar';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 
 
-const products = [
+
+const slides = [
   {
-    title: "Moonbeam",
-    link: "https://gomoonbeam.com",
-    thumbnail:
-      "/1.png",
+    title : "Global Sourcing, Local Expertise",
+    subtitle : "Equipment Solutions for Every Need.",
+    image : "/1.png"
   },
   {
-    title: "Cursor",
-    link: "https://cursor.so",
-    thumbnail:"/2.png",
+   title : "Build the Kitchen of Your Dreams",
+   subtitle : "Trusted Partner from Start to Finish.",
+   image : "/2.png" 
   },
   {
-    title: "Rogue",
-    link: "https://userogue.com",
-    thumbnail:
-      "/3.png",
-  },
- 
-  {
-    title: "Editorially",
-    link: "https://editorially.org",
-    thumbnail:
-      "/4.jpg",
+    title : "Crafting Culinary Dreams, One Kitchen at a Time",
+    subtitle : "Innovative Solutions for Every Budget.",
+    image : "/3.png"
   },
   {
-    title: "Editrix AI",
-    link: "https://editrix.ai",
-    thumbnail:
-      "/5.jpg",
-  },
-  {
-    title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
-    thumbnail:
-      "/6.jpg",
-  },
- 
-  {
-    title: "Algochurn",
-    link: "https://algochurn.com",
-    thumbnail:
-      "/7.jpg",
-  },
-  {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "/8.jpg",
-  },
-  {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "/9.jpg",
-  },
-  {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "/10.jpg",
-  },
-  {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "/11.jpg",
-  },
-  {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "/12.jpg",
-  },
-];
+    title : "Expert Design & Installation",
+    subtitle : "Your One-Stop Shop for Commercial Kitchens.",
+    image : "/4.jpg"
+  }
+]
 
 
 
@@ -86,9 +41,24 @@ export default function Home() {
     <main className="bg-background text-foreground ">
       <Navbar />
 
-      <HeroParallax products = {products}/>
+      {/* <HeroParallax products = {products}/> */}
       
-      <section className='w-full h-screen hero -mt-20'></section>
+      <section className='w-full h-screen'>
+      <Swiper pagination={true} modules={[Pagination]} className="h-[90vh]">
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index} className="h-full">
+            <div className="h-full w-full bg-cover bg-center bg-no-repeat" 
+            style={{backgroundImage: `url(${slide.image})`}}>
+              <div className='absolute top-0 left-0 z-[5] bg-blue-950 w-full h-full opacity-55'/>
+              <div className="h-full w-full flex flex-col justify-center items-center text-center space-y-5">
+                <h1 className="text-6xl font-bold text-white z-10">{slide.title}</h1>
+                <p className="text-2xl text-white z-10">{slide.subtitle}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      </section>
       {/* <h1 className="text-4xl text-gradient">hello</h1> */}
 
     </main>
